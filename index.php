99f7f67c61daea8a1e59f6a5023d309d2779bce2
Gurdeep Singh <gurdeepmundi10@gmail.com>
2022-02-20 07:21:48 -0700

    updated design and integration

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <html>
 <head>
 <!--- link to the styles file -->
-<link rel="stylesheet" type="text/css" href="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <?php
@@ -20,5 +21,33 @@
         if(!$res)
         echo mysqli_error($link);*/
     ?>
+    <div class="mediator"> <button><a href="login.html"><b>Trained Professional LOGIN</b></a></button></div>
+  
+  <div class="topnav">
+<div class="search-container">
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <input type="text" placeholder="Search.." name="search">
+    <button type="submit"><i class="fa fa-search"></i><b >SEARCH</b></button>
+  </form>
+</div>
+</div>
+<div class = "main-body">
+    <div class = "item4">
+<a href=""> <img src="despondent-gambler-losing-at-the-casino.jpg" width = 80%> </a>
+<h3 align ="center">Gambling</h3>
+</div>
+    <div class = "item1">
+<a href=""> <img src="despondent-gambler-losing-at-the-casino.jpg" width = 80%> </a>
+<h3 align ="center">Alcoholism</h3>
+    </div>
+    <div class = "itme2">
+<a href=""> <img src="despondent-gambler-losing-at-the-casino.jpg" width = 80%> </a>
+<h3 align ="center">Gaming</h3>
+    </div>
+    <div class = "item 3">
+<a href=""> <img src="despondent-gambler-losing-at-the-casino.jpg" width = 80%> </a>
+<h3 align ="center">Smoking</h3>
+    </div>
+  <div class="footer"></div>
 </body>
 </html>
